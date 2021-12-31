d7d5b870a77ec162b3534ffc79e6f26781a9449d
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-31 21:20:18 +0300

    feat: React native maps implemented

--- src/pages/newActivity/NewActivityPage.js
+++ src/pages/newActivity/NewActivityPage.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
+import MapView from 'react-native-maps'
+import styles from "./NewActivityPageStyles";
 
 const NewActivityPage = () => {
   return(
-    <SafeAreaView>
-      <Text>New Activity Page</Text>
+    <SafeAreaView style={styles.outerContainer}>
+      <MapView 
+        showUserLocation={true}
+        style={styles.mapView}></MapView>
+      <View style={styles.container}></View>
     </SafeAreaView>
   )
 }
---
+++ src/pages/newActivity/NewActivityPageStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native"
+
+
+const styles = StyleSheet.create({
+  outerContainer:{
+    flex:1
+  },
+  mapView:{
+    flex:1
+  },
+  container:{
+    flex:1
+  }
+})
+
+export default styles
