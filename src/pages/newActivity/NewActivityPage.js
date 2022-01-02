df3183f05b42cb39dc330eb3002048d7d720824b
yavuz <yavuz.akpinar.68@gmail.com>
2022-01-03 00:52:43 +0300

    feat: User location can be tracked now

--- src/pages/newActivity/NewActivityPage.js
+++ src/pages/newActivity/NewActivityPage.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Dimensions, SafeAreaView, Text, View } from "react-native";
-import MapView, { Marker } from 'react-native-maps'
+import MapView, { Marker, Polyline } from 'react-native-maps'
 import styles from "./NewActivityPageStyles";
 import Geolocation from "@react-native-community/geolocation";
 import Button from "../../components/button/Button";
-
+import database from '@react-native-firebase/database'
 
 
 const NewActivityPage = () => {
   const [location, setLocation] = useState()
-  const [watchLocation, setWatchLocation] = useState()
+  const [watchLocation, setWatchLocation] = useState([])
+  const [finish, setFinish] = useState()
 
   const initialRegion = {
     latitude: 41.0391683,
@@ -18,39 +19,77 @@ const NewActivityPage = () => {
     longitudeDelta: 0.02,
   };
 
- 
-  Geolocation.watchPosition(
-    (position) => {
-      console.log('watch.position', position);
-      setWatchLocation(position.coords);
-    },
-    (error) => {
-      console.log(error);
-    },
-  );
 
-  const handleLocation = () => {
+  const handleStart = () => {
     Geolocation.getCurrentPosition(
       (position) => {
-        console.log('get.position', position);
+        console.log('get.position', location);
         setLocation(position.coords);
+      },
+      (error) => {
+        console.log(error);
+      })
+    console.log(watchLocation)
+
+  }
+
+  const handleFinish = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        console.log('finish.position', position);
+        setFinish(position.coords);
       },
       (error) => {
         console.log(error);
       })
   }
 
+  const handleClear = () => {
+    setLocation()
+    setFinish()
+    setWatchLocation([])
+  }
+
+  /*Geolocation.watchPosition(
+    (position) => {
+      setWatchLocation([{latitude:position.coords.latitude, longitude:position.coords.longitude}, ...watchLocation])
+      console.log('position',watchLocation)
+    },
+    (error) => {
+      console.log(error);
+    },
+    {enableHighAccuracy:true,
+    distanceFilter:10
+    }
+  );*/
+
+  const handleUserTracking = (e) => {
+    console.log("aaaa", e.nativeEvent)
+          setWatchLocation([{ 
+            latitude: e.nativeEvent.coordinate.latitude, 
+            longitude: e.nativeEvent.coordinate.longitude }, 
+            ...watchLocation])
+  }
 
   return (
     <SafeAreaView style={styles.outerContainer}>
       <MapView
         style={styles.mapView}
         initialRegion={initialRegion}
-        showsUserLocation={true}>
-          {location !== undefined && <Marker coordinate={location}></Marker>}
+        showsUserLocation={true}
+        userLocationPriority="high"
+        onUserLocationChange={finish === undefined && location && handleUserTracking}>
+        {location !== undefined && <Marker coordinate={location}></Marker>}
+        {finish !== undefined && <Marker coordinate={finish}></Marker>}
+        {watchLocation !== undefined && location && <Polyline
+        miterLimit={10}
+        lineCap="square" strokeWidth={4} strokeColor="blue" coordinates={watchLocation}></Polyline>}
       </MapView>
       <View style={styles.container}>
-        <Button onPress={handleLocation}></Button>
+        <Button title="Start" onPress={handleStart} ></Button>
+        <Button title="Finish" onPress={handleFinish}></Button>
+        <Button title="Clear" onPress={handleClear}></Button>
+        <Button title="Clear" onPress={e => console.log("aaaa", e.onUserLocationChange)}></Button>
       </View>
     </SafeAreaView>
   )
